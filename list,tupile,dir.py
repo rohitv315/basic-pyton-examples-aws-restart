1318f32b6ee6b20831b41f562977d4647725d344
ROHIT RAMESH GEDAM <100556673+rohitv315@users.noreply.github.com>
2023-03-31 12:53:54 +0530

    Create list,tupile,dir.py

---
+++ list,tupile,dir.py
@@ -0,0 +1,25 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+myFruitList[0] = "orange"
+print(myFruitList)
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[1])
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+print(myFavoriteFruitDictionary["Akua"])
+
+
